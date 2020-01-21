5ce3d58bf7595338167711a7b9e50467cd08dae3
krumaska <51515055+krumaska@users.noreply.github.com>
2020-01-22 01:21:32 +0900

    Update loginPage.dart include krDescription & validator

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -32,14 +32,58 @@ class LoginScreen extends StatelessWidget {
     });
   }
 
+  String pwValidator (String pw) {
+    if(pw.length>=5 && pw.length<=10) {
+      return null;
+    } else return '비밀번호가 너무 짧습니다 (최소 5자리 이상 10자리 이하)';
+  }
+  
+  String emailValidator (String email) {
+    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+    if (emailValid == false) {
+      return '올바른 이메일 형식이 아닙니다';
+    } else return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
-    
-      theme: LoginTheme( 
+      passwordValidator: pwValidator,
+      emailValidator: emailValidator,
+      messages: LoginMessages(
+        usernameHint: '이메일',
+        passwordHint: '비밀번호',
+        confirmPasswordHint: '비밀번호 확인',
+        forgotPasswordButton: '비밀번호 찾기',
+        loginButton: '로그인',
+        signupButton: '회원가입',
+        recoverPasswordButton: '전송',
+        recoverPasswordIntro: '가입하신 이메일을 적어주세요',
+        recoverPasswordDescription: '작성하신 이메일로 비밀번호가 전송됩니다!',
+        goBackButton: '뒤로',
+        confirmPasswordError: '비밀번호가 일치하지 않습니다',
+        recoverPasswordSuccess: '비밀번호 전송성공'
+      ),
+      theme: LoginTheme(
+        titleStyle: TextStyle(
+          fontWeight: FontWeight.bold
+        ),
+        inputTheme: InputDecorationTheme(
+          fillColor: Colors.black12,
+          filled: true,
+          labelStyle: TextStyle(color: Colors.black, fontSize: 20.0),
+        ),
+        buttonTheme: LoginButtonTheme(
+          splashColor: Colors.white
+        ),
         accentColor: Colors.white,
-        textFieldStyle: TextStyle(color: Colors.white),
         beforeHeroFontSize: 50.0,
+        bodyStyle: TextStyle(
+          color: Colors.black
+        ),
+        cardTheme: CardTheme(
+          color: Colors.white
+        )
       ),
       title: 'JARAM',
       logo: 'assets/images/Jaram.png',
